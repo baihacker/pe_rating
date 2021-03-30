c34ff3ffa59507418fadcda3945f4590cccd03ed
baihacker <bailiangsky@gmail.com>
2021-03-29 21:22:28 -0700

    1. Update pe db. 2. Move fetch/fetch.py to fetch.py 3. Format fetch.py.

--- fetch.py
+++ fetch.py
@@ -1,31 +1,38 @@
+#! python
 import urllib2
 import time
 import socket
+import os
+
+SCRIPT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 
 socket.setdefaulttimeout(60)
 
-cookie=''
-minid=752
-maxid=752
+cookie = ''
+minid = 753
+maxid = 753
+
 
 def fetch(id):
   opener = urllib2.build_opener()
   opener.addheaders.append(('Cookie', cookie))
-  url = "https://projecteuler.net/fastest=%s"%id
+  url = "https://projecteuler.net/fastest=%s" % id
   f = opener.open(url)
-  result = f.read().decode(encoding='utf8',errors='ignore')
+  result = f.read().decode(encoding='utf8', errors='ignore')
   return result
 
-def fetch_all():
-  for i in range(minid, maxid+1):
 
-    filename = 'pe%d.txt'%i
+def fetch_all():
+  for i in range(minid, maxid + 1):
+
+    filename = os.path.join(SCRIPT_DIRECTORY, 'data\\pe\\pe%d.txt' % i)
     print(filename)
 
     result = fetch(i)
     with open(filename, 'wb') as tempf:
       tempf.write(result)
     time.sleep(1)
+
 
 if __name__ == "__main__":
   fetch_all()
